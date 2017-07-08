0107607f14ded9841bcb89193d2ffc7a90d2924a
kanadenipun <nipunk@thoughtworks.com>
2017-07-08 16:26:08 +0530

    Added table view with same data

--- emoji-dicitionary/ViewController.swift
+++ emoji-dicitionary/ViewController.swift
@@ -8,18 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var dictionaryTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        dictionaryTableView.dataSource = self as! UITableViewDataSource
+        dictionaryTableView.delegate = self as! UITableViewDelegate
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Hello There"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
